b4e0cef169144de88e85190489c81deeb1826514
dugucrypter <bonzime@gmail.com>
2023-01-15 01:33:43 +0000

    add: packagind setup config

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    packages=find_packages(include=['georunes*'], ),
+    python_requires=">=3.6",
+    install_requires=["matplotlib",
+                      "numpy",
+                      "scipy",
+                      "pandas"],
+    include_package_data=True,
+    package_data={"georunes": ["data/*.txt", "data/*.csv"]}
+)
